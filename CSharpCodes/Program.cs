1335cd6a26ea011382867f6051f7ee75974ba0e0
PureJoyMind <mazdak.parniandg@gmail.com>
2023-04-23 18:38:21 +0430

    Added delegates and implementation

--- CSharpCodes/Program.cs
+++ CSharpCodes/Program.cs
@@ -2,6 +2,7 @@
 
 //using System.Text;
 
+using Delegates;
 using System;
 using System.Collections.Generic;
 
@@ -13,7 +14,18 @@ namespace CSharpCodes
     {
         static void Main(string[] args)
         {
-            
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+            processor.Process("photo.jpg", filterHandler);
+        }
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
+
         }
     }
 }
