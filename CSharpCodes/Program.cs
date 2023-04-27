e12f15a1a73e6ded05868635aa62b1877293ec90
PureJoyMind <mazdak.parniandg@gmail.com>
2023-04-27 19:40:27 +0430

    minor

--- CSharpCodes/Program.cs
+++ CSharpCodes/Program.cs
@@ -5,27 +5,35 @@
 using Delegates;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpCodes
 {
-    
-    
     internal class Program
     {
+        public class Book
+        {
+            public string name;
+            public int price;
+        }
         static void Main(string[] args)
         {
-            var processor = new PhotoProcessor();
-            var filters = new PhotoFilters();
-            Action<Photo> filterHandler = filters.ApplyBrightness;
-            filterHandler += filters.ApplyContrast;
-            filterHandler += RemoveRedEyeFilter;
+            //var processor = new PhotoProcessor();
+            //var filters = new PhotoFilters();
+            //Action<Photo> filterHandler = filters.ApplyBrightness;
+            //filterHandler += filters.ApplyContrast;
+            //filterHandler += RemoveRedEyeFilter;
 
-            processor.Process("photo.jpg", filterHandler);
-        }
-        static void RemoveRedEyeFilter(Photo photo)
-        {
-            Console.WriteLine("Apply RemoveRedEye");
+            //processor.Process("photo.jpg", filterHandler);
 
-        }
+
+            //delegate bool ISBigger(int args, int b);aa
+
+            }
+        //static void RemoveRedEyeFilter(Photo photo)
+        //{
+        //    Console.WriteLine("Apply RemoveRedEye");
+
+        //}
     }
 }
